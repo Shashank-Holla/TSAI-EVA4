7c7768740a3fbb711565a22f3b18ed900d927b0c
Shashank-Holla <58994044+Shashank-Holla@users.noreply.github.com>
2020-04-01 03:20:38 +0530

    pytorch package transformation

---
+++ Session10_LearningRate/transform.py
@@ -0,0 +1,24 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+def transform():
+    
+    #
+    transform = transforms.Compose(
+      [transforms.RandomCrop(32, padding=4),
+       transforms.RandomHorizontalFlip(),
+       transforms.ToTensor(),
+       transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                          download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=128,
+                                            shuffle=True, num_workers=4)
+
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                        download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=128,
+                                          shuffle=False, num_workers=4)
+
+    classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+    return trainloader, testloader, classes

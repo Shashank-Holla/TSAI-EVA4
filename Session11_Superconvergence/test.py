5dbc1a42834e95745166c7e4458970d870f2137b
Shashank-Holla <58994044+Shashank-Holla@users.noreply.github.com>
2020-04-12 04:46:47 +0530

    Add files for session 11

---
+++ Session11_Superconvergence/test.py
@@ -0,0 +1,25 @@
+import torch
+def test(net, device, testloader, criterion): 
+    correct = 0
+    total = 0
+    epoch_test_loss = 0.0
+    epoch_test_accuracy = 0
+    net.eval()
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            images = images.to(device)
+            labels = labels.to(device)
+            outputs = net(images)
+
+            epoch_test_loss += criterion(outputs, labels).item()
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+            
+    epoch_test_accuracy = (100 * correct / total)
+    epoch_test_loss /= len(testloader)
+    # print("Test loss=",epoch_test_loss)
+    # print('Accuracy of the network on the 10000 test images: %d %%' %epoch_test_accuracy)
+    print('Epoch Test loss:',epoch_test_loss,'        Epoch Test Accuracy:',epoch_test_accuracy)
+    return epoch_test_accuracy, epoch_test_loss
